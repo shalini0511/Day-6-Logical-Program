c756ab0a6f7f158acad3f7dd8f8e2289ba298eb0
shalini <shalini.v@tvsnext.io>
2021-07-01 20:52:49 +0530

    [shalini]Days Of Week

--- Day-6-PracticeProgram/Program.cs
+++ Day-6-PracticeProgram/Program.cs
@@ -7,35 +7,49 @@ using System.Threading;
 namespace Day_6_PracticeProgram
 {
     class program
-{
-    public static void Main(string[] args)
     {
-        Input.MonthlyPayment();
-        Console.ReadLine();
-
-    }
-}
-    class Input
-    {
-        public static void MonthlyPayment()
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Enter principle");
-            double p = Convert.ToInt32(Console.ReadLine());
+            DaysOfWeek.ReadInput();
+            Console.ReadLine();
 
-            Console.WriteLine("Enter years");
-            int y = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Enter rate");
-            double R = Convert.ToInt32(Console.ReadLine());
-            int n = 12 * y;
-            double r = (R / (12 * 100));
-            double s = 1 + r;
-            double power = (double)Math.Pow(s, -n);
-            double payment = (p * r) / power;
-            Console.WriteLine("Monthly payment is :{0}", payment);
         }
+    }
+    class Utility
+    {
+        public static int ReadInput()
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+            return input;
+        }
+    }
+    class DaysOfWeek
+    {
 
-
+        public static void ReadInput()
+        {
+            //Get input from Utility Class
+            Console.WriteLine("Enter the date(1-30)");
+            int d = Utility.ReadInput();
+            Console.WriteLine("Enter the month (1-12)");
+            int m = Utility.ReadInput();
+            Console.WriteLine("Enter the year(yyyy)");
+            int y = Utility.ReadInput();
+            //Call DayCalculate Function
+            DayCalculate(d, m, y);
+        }
+        private static void DayCalculate(int d, int m, int y)
+        {
+            //List to print the week
+            List<string> day = new List<string>() { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+            //Given formula: Gregorian calendar
+            int y0 = y - (14 - m) / 12;
+            int x = y0 + y0 / 4 - y0 / 100 + y0 / 400;
+            int m0 = m + 12 * ((14 - m) / 12) - 2;
+            int d0 = (d + x + 31 * m0 / 12) % 7;
+            //Print day of week 
+            Console.WriteLine("Day is :" + day[d0]);
+        }
 
 
     }
