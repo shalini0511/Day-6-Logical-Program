c46819482da130d1a2863f7798028a6cda0e31a0
shalini <shalini.v@tvsnext.io>
2021-07-01 19:14:50 +0530

    [shalini]Temperature conversion

--- Day-6-PracticeProgram/Program.cs
+++ Day-6-PracticeProgram/Program.cs
@@ -6,29 +6,28 @@ using System.Threading;
 
 namespace Day_6_PracticeProgram
 {
-    class Program
+    class TemperatureConversion
     {
-        static void Main(string[] args)
+        double celsius;
+        double fahrenheit;
+        static void Main (string[] args)
         {
-            int couponStart, couponEnd;
-            //Get range for coupon from user
-            Console.WriteLine("Enter start range of coupon number");
-            couponStart = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter End range of coupon number");
-            couponEnd = Convert.ToInt32(Console.ReadLine());
-            Random random = new Random();
-            int randomNumber = random.Next(couponStart, couponEnd);
-            List<int> number = new List<int>();
-            number.Add(randomNumber);
-            List<int> distinct = number.Distinct().ToList();
-            foreach (int i in distinct)
-            {
-                Console.WriteLine(" Distinct coupon nummber is :" + i);
-            }
+            double celsius;
+            double fahrenheit;
+            Console.WriteLine("Enter value for celsius:");
+            celsius = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter value for Fahrenheit:");
+            fahrenheit = Convert.ToDouble(Console.ReadLine());
+            double celToFah = (celsius * 9 / 5) + 32;
+            double fahToCel = (fahrenheit - 32) * 5 / 9;
+            Console.WriteLine("After celsius to fahrenheit conversion: {0}", celToFah);
+            Console.WriteLine("After fahrenheit to celsius conversion: {0}", fahToCel);
+
         }
+
     }
+    
 }
 
 
 
-          
