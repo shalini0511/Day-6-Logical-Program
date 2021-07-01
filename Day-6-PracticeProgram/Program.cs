243aef0447e14c2b9a1ec6805095d62b61da2173
shalini <shalini.v@tvsnext.io>
2021-07-01 15:18:34 +0530

    [shalini]Stop Watch

---
+++ Day-6-PracticeProgram/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Day_6_PracticeProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create new stopwatch
+            Stopwatch stopWatch = new Stopwatch();
+            //Begin Timimg
+            stopWatch.Start();
+            //do something
+            for (int i=0;i<1000;i++)
+            {
+                Thread.Sleep(1);
+            }
+            //stop timing
+            stopWatch.Stop();
+            //printing result
+            Console.WriteLine("Time Elapsed:{0}",stopWatch.Elapsed);
+            
+        }
+    }
+}
